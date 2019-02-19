f2d994cd5520cb9ec8c077c42ba97053a1d09407
alexandr.nemankov <alexandr.nemankov@triboo.it>
2019-02-19 17:26:30 +0300

    task solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var numberOfLovers = preferences.length,
+        counter = 0,
+        numberOfTriangles;
+
+    for (var i = 0; i < numberOfLovers; i++) {
+        var first = preferences[i] - 1,
+            second = preferences[first] - 1,
+            third = preferences[second];
+
+
+        if (third === i + 1  && preferences[i] !== i+1) {
+            counter++;
+        }
+    }
+
+    numberOfTriangles = counter / 3 ;
+
+    return numberOfTriangles;
 };
